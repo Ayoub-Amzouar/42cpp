d7ef538216a9f4192876dc0d048735cc19c2ae70
Ayoub Amzouar <aamzouar@e1r13p7.1337.ma>
2021-11-09 10:26:12 +0100

    megaphone program is done

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <iostream>
+#include <cctype>
 
 int		main(int ac, char *av[])
 {
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j] != '\0'; j++)
+				av[i][j] = toupper(av[i][j]);
+			std::cout << av[i];
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
