9007ae0ce6013e21aba15bd68abdf4ce9e3b2bd3
Ayoub Amzouar <aamzouar@e2r5p11.1337.ma>
2021-11-15 18:23:50 +0100

    ex02 is completed

--- day01/ex02/main.cpp
+++ day01/ex02/main.cpp
@@ -2,5 +2,19 @@
 
 int		main(void)
 {
+	std::string		brain;
+	std::string*	stringPTR;
+	std::string&	stringREF = brain;
+
+	brain = "HI THIS IS BRAIN";
+	stringPTR = &brain;
+	std::cout << "------------------------------------------------" << std::endl;
+	std::cout << "Address of brain: " << &brain << std::endl;
+	std::cout << "Address of brain using stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of brain using stringREF: " << &brain << std::endl;
+	std::cout << "------------------------------------------------" << std::endl;
+	std::cout << "Brain displyed using stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Brain displyed using stringREF: " << stringREF << std::endl;
+	std::cout << "------------------------------------------------" << std::endl;
 	return (0);
 }
