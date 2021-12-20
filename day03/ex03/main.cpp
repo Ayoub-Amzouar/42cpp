324c8469f1d505c5e5376ccf38b740a7460d48f8
Ayoub Amzouar <aamzouar@e1r2p1.1337.ma>
2021-12-20 15:41:27 +0100

    adding more tests

--- day03/ex03/main.cpp
+++ day03/ex03/main.cpp
@@ -5,34 +5,36 @@
 
 int		main(void)
 {
-	ClapTrap	obj1;
-	ScavTrap	obj2;
-	FragTrap	obj3;
+	// ClapTrap	obj1;
+	// ScavTrap	obj2;
+	// FragTrap	obj3;
 	DiamondTrap	obj4;
 
-	std::cout << "-------------- LEVEL 0 --------------"<< std::endl;
+	// std::cout << "-------------- LEVEL 0 --------------"<< std::endl;
 	
-	obj1 = ClapTrap("\033[1;32mEren\033[0m");
-	obj1.attack("\033[1;34mReiner\033[0m");
-	obj1.takeDamage(13);
-	obj1.beRepaired(5);
+	// obj1 = ClapTrap("\033[1;32mEren\033[0m");
+	// obj1.attack("\033[1;34mReiner\033[0m");
+	// obj1.takeDamage(13);
+	// obj1.beRepaired(5);
 	
-	std::cout << "-------------- LEVEL 1 --------------"<< std::endl;
+	// std::cout << "-------------- LEVEL 1 --------------"<< std::endl;
 
-	obj2 = ScavTrap("\033[1;32mMikasa\033[0m");
-	obj2.attack("\033[1;34mAnnie\033[0m");
-	obj2.guardGate();
+	// obj2 = ScavTrap("\033[1;32mMikasa\033[0m");
+	// obj2.attack("\033[1;34mAnnie\033[0m");
+	// obj2.guardGate();
 	
-	std::cout << "-------------- LEVEL 2 --------------"<< std::endl;
+	// std::cout << "-------------- LEVEL 2 --------------"<< std::endl;
 	
-	obj3 = FragTrap("\033[1;32mLevi\033[0m");
-	obj3.highFivesGuys();
+	// obj3 = FragTrap("\033[1;32mLevi\033[0m");
+	// obj3.highFivesGuys();
 	
 	std::cout << "-------------- LEVEL 3 --------------"<< std::endl;
 	
 	obj4 = DiamondTrap("\033[1;32mArmin\033[0m");
 	obj4.whoAmI();
 	obj4.attack("\033[1;34mBertholdt\033[0m");
+	obj4.guardGate();
+	obj4.highFivesGuys();
 	
 	return (0);
 }
