97ff451c11002c72b90e8e1d4c66b04977fc0e62
Ayoub Amzouar <aamzouar@e1r2p1.1337.ma>
2021-12-22 12:20:13 +0100

    adding more tests

--- day04/ex00/main.cpp
+++ day04/ex00/main.cpp
@@ -5,20 +5,25 @@
 
 int		main(void)
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	WrongAnimal*  k = new WrongCat();
+	const Animal*	meta = new Animal();
+	const Animal*	j = new Dog();
+	const Animal*	i = new Cat();
+	WrongAnimal*	k = new WrongCat();
+	WrongCat		y;
 
+	std::cout << "-------------- Type --------------" << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 
+	std::cout << "-------------- makeSound --------------" << std::endl;
 	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
 
 	k->makeSound();
+	y.makeSound();
 
+	std::cout << "-------------- Destructors --------------" << std::endl;
 	delete meta;
 	delete j;
 	delete i;
