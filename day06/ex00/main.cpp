b695a035d3224e92f1f635ec95c97c0b3122f4dc
sesco <sesco@example.com>
2021-12-24 23:34:15 +0100

    there's still a minor error when check if the input is unvalid

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 /*
 	stoa()
@@ -30,11 +31,14 @@ void	print_valid( std::string input )
 {
 	double	nb;
 
-	nb = std::stod(input);
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: " << input << "f" << std::endl;
-	std::cout << "double: " << input << std::endl;
+	nb = atof(input.c_str());
+	if (nb >= 0 && nb <= 15)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(nb) << std::endl;
+	std::cout << "int: " << static_cast<int>(nb) << std::endl;
+	std::cout << "float: " << static_cast<float>(nb) << std::endl;
+	std::cout << "double: " << static_cast<float>(nb) << std::endl;
 }
 
 void	check_input( char *input )
@@ -46,10 +50,10 @@ void	check_input( char *input )
 		print_pseudo_f(s_input);
 	else if (s_input == "-inf" || s_input == "+inf" || s_input  == "nan")
 		print_pseudo_d(s_input);
-	else if (std::stod(s_input))
+	else if (s_input.find_first_not_of("1234567890.f") == std::string::npos)
 		print_valid(s_input);
-	// else
-	// 	std::cerr << "unvalid input!" << std::endl;
+	else
+		std::cerr << "unvalid input!" << std::endl;
 }
 
 int		main( int ac, char *av[] )
