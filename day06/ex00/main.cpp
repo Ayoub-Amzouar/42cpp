0b098e211b651a77d08f3fb4d51767d4c843ba4e
Ayoub Amzouar <aamzouar@e1r13p7.1337.ma>
2021-12-24 17:51:19 +0100

    still working

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,6 +6,37 @@
 	stod()
 */
 
+void	print_pseudo_f( std::string input )
+{
+	int		len;
+
+	len = input.length() - 1;
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << input << std::endl;
+	input.resize(len, 1);
+	std::cout << "double: " << input << std::endl;
+}
+
+void	print_pseudo_d( std::string input )
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << input << "f" << std::endl;
+	std::cout << "double: " << input << std::endl;
+}
+
+void	print_valid( std::string input )
+{
+	double	nb;
+
+	nb = std::stod(input);
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << input << "f" << std::endl;
+	std::cout << "double: " << input << std::endl;
+}
+
 void	check_input( char *input )
 {
 	std::string	s_input;
@@ -17,8 +48,8 @@ void	check_input( char *input )
 		print_pseudo_d(s_input);
 	else if (std::stod(s_input))
 		print_valid(s_input);
-	else
-		std::cerr << "unvalid input!" << std::endl;
+	// else
+	// 	std::cerr << "unvalid input!" << std::endl;
 }
 
 int		main( int ac, char *av[] )
