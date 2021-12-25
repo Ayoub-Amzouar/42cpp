657c16c3a4c84fb09eaa4089a6978b1b3fc16e58
sesco <sesco@example.com>
2021-12-25 16:22:30 +0100

    fixing precision problem in cout

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <stdlib.h>
+#include <sstream>
+#include <iomanip>
 
 /*
 	stoa()
@@ -30,15 +31,17 @@ void	print_pseudo_d( std::string input )
 void	print_valid( std::string input )
 {
 	double	nb;
+	std::stringstream	stream;
 
-	nb = atof(input.c_str());
-	if (nb >= 0 && nb <= 15)
+	stream << input;
+	stream >> nb;
+	if (isprint(nb) == false)
 		std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: " << static_cast<char>(nb) << std::endl;
 	std::cout << "int: " << static_cast<int>(nb) << std::endl;
-	std::cout << "float: " << static_cast<float>(nb) << std::endl;
-	std::cout << "double: " << static_cast<float>(nb) << std::endl;
+	std::cout << "float: " << static_cast<float>(nb) << (nb != (int)nb ? "" : ".0") << 'f' << std::endl;
+	std::cout << "double: " << static_cast<float>(nb) << (nb != (int)nb ? "" : ".0") << std::endl;
 }
 
 void	check_input( char *input )
@@ -50,7 +53,7 @@ void	check_input( char *input )
 		print_pseudo_f(s_input);
 	else if (s_input == "-inf" || s_input == "+inf" || s_input  == "nan")
 		print_pseudo_d(s_input);
-	else if (s_input.find_first_not_of("1234567890.f") == std::string::npos)
+	else if (s_input.find_first_not_of("1234567890.f") == std::string::npos) 
 		print_valid(s_input);
 	else
 		std::cerr << "unvalid input!" << std::endl;
