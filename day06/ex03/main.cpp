017c1759741eb5216b987edbbfd405662cb3a2e5
Ayoub Amzouar <gramgeek01@gmail.com>
2022-01-19 16:47:47 +0100

    done!

--- day06/ex03/main.cpp
+++ day06/ex03/main.cpp
@@ -38,11 +38,42 @@ void	identify( Base* p )
 		std::cout << "C" << std::endl;
 }
 
+void	identify( Base& p )
+{
+	try
+	{
+		Base& var = dynamic_cast<A&>(p);
+		(void)var;
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		try
+		{
+			Base& var = dynamic_cast<B&>(p);
+			(void)var;
+			std::cout << "B" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			try
+			{
+				Base& var = dynamic_cast<C&>(p);
+				(void)var;
+				std::cout << "C" << std::endl;
+			}
+			catch(...) {}
+		}
+	}
+}
+
 int     main( void )
 {
-	Base*	p;
+	Base*			p;
+	Base* const &	r = generate();
 
 	p = generate();
 	identify(p);
+	identify(*r);
 	return (0);
 }
