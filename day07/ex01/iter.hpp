2e9faff7b8a3c9ca77db4de063e05b04f6689fd9
Ayoub Amzouar <gramgeek01@gmail.com>
2022-01-22 17:21:35 +0100

    exercise 01 day07 is completed

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -1,11 +1,11 @@
 #ifndef __ITER_HPP__
 #define __ITER_HPP__
 
-template<class T>
+template<typename T>
 void	iter(T **arr, int len, void (*fun_ptr)(T))
 {
 	for (int i = 0; i < len; i++)
-		(*fun_ptr)(*arr[i]);
+		(*fun_ptr)((*arr)[i]);
 }
 
 #endif
---
+++ day07/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include "iter.hpp"
+#include <iostream>
+
+void	print_arr( int nb )
+{
+	std::cout << nb << std::endl;
+}
+
+int		main( void )
+{
+	int	*arr = new int[4];
+
+	for (int i = 0; i < 4; i++)
+		arr[i] = i + 1;
+	
+	iter(&arr, 4, &print_arr);
+	delete [] arr;
+	return (0);
+}
