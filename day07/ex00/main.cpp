2b4246e89e9e293ee06b6b9bd5cc51d5873d1108
Ayoub Amzouar <aamzouar@e1r6p9.1337.ma>
2022-02-01 17:03:27 +0100

    remove those :: from swap function calls´

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -7,19 +7,21 @@ int main(void)
 {
 	int a = 2;
 	int b = 3;
+
+	std::cout << "initial a = " << a << " and b = " << b << "\n\n";
+	swap(a, b);
 	
-	::swap(a, b);
-	
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "a & b swapped a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
-	::swap(c, d);
+	std::cout << "initial c = " << c << " and d = " << d << "\n\n";
+	swap(c, d);
 	
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "c & d swapped c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return 0;
