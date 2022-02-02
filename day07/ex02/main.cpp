8a86cbecb9b7128c77d3a541b1129a6ffe24f842
Ayoub Amzouar <aamzouar@e1r6p11.1337.ma>
2022-02-02 15:34:18 +0100

    adding more tests in main

--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,18 +1,44 @@
 #include "Array.hpp"
 #include <iostream>
 
+#define MAX_VAL 10
+
 int		main( void )
 {
-	Array<char>	arr2;
+	Array<int>	arr1(MAX_VAL);
+
+	std::cout << "arr1 elements:" << std::endl;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		arr1[i] = i + 1;
+		std::cout << arr1[i] << " ";
+	}
+	std::cout << std::endl;
+
+	Array<int>	arr2 = arr1;
+	arr2[3] = -1;
+	std::cout << "arr2 elements:" << std::endl;
+	for (int i = 0; i < MAX_VAL; i++)
+		std::cout << arr2[i] << " ";
+	std::cout << std::endl;
 
 	try
 	{
-		arr2[0] = 'c';
-		std::cout << arr2[0] << std::endl;
+		Array<char> arr3;
+		std::cout << arr3[3];
 	}
 	catch (std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+	try
+	{
+		std::cout << arr2[MAX_VAL + 10];
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return (0);
 }
