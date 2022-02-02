154992b7b880707688ae08854ec81c5fabfaff5f
Ayoub Amzouar <aamzouar@e1r6p11.1337.ma>
2022-02-02 13:54:46 +0100

    fixing a lot of errors like how copy constructor and assignation operator works

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -1,37 +1,70 @@
 #ifndef __ARRAY_HPP__
 #define __ARRAY_HPP__
 
+#include <iostream>
+
 template<typename T>
 class Array
 {
 public:
 	Array( void )
 	{
-		this->_a = new T[0];
+		this->_arr = NULL;
 		this->_size = 0;
 	}
 	Array( unsigned int n ) 
 	{
-		this->_a = new T[n];
+		this->_arr = new T[n];
 		this->_size = n;
+		for (int i = 0; i < n; i++)
+			this->_arr[i] = int();
 	}
 	Array( Array const &copy )
 	{
-		for (int i = 0; i < _size; i++)
-			this->_a[i] = copy._a[i];
+		this->_size = copy.size();
+		this->_arr = new T[this->_size];
+		for (int i = 0; i < this->_size; i++)
+			this->_arr[i] = copy._arr[i];
 	}
 	Array& operator=( Array const &rop )
 	{
 		if (&rop == this)
 			return (this);
-		for (int i = 0; i < _size; i++)
-			this->_a[i] = copy._a[i];
+		if (this->_size < rop.size())
+		{
+			delete [] this->_arr;
+			this->_arr = new T[rop.size()];
+			this->_size = rop.size();
+		}
+		for (int i = 0; i < rop.size(); i++)
+			this->_arr[i] = rop._arr[i];
 		return (*this);
 	}
-	~Array( void ) { delete [] this->_a; }
+	T&	operator[]( int i )
+	{
+		if (i > this->_size)
+		{
+			throw IndexOutOfBounds();
+			return (this->_arr[0]);
+		}
+		return (this->_arr[i]);
+	}
+	class	IndexOutOfBounds : public std::exception
+	{
+		public:
+			const char *what( void ) const throw()
+			{
+				return ("Error: Index Out Of Bounds");
+			}
+	};
+	~Array( void )
+	{
+		if (this->_size != 0)
+			delete [] this->_arr;
+	}
 	int		size( void ) { return (this->_size); }
 private:
-	T	*_a;
+	T	*_arr;
 	int	_size;
 };
 
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,9 +1,18 @@
-#include "Array.h"
+#include "Array.hpp"
 #include <iostream>
 
 int		main( void )
 {
-	Array<int>	arr1;
-	Array<int>	arr2(6);
+	Array<char>	arr2;
+
+	try
+	{
+		arr2[0] = 'c';
+		std::cout << arr2[0] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
